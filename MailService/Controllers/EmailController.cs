3115379dbd22599b35d0a924a722e310d5b55386
charbel <charbel-boumaroun@outlook.com>
2021-01-13 15:03:13 +0200

    feat: Added Mail Service

---
+++ MailService/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using MailService.Dtos;
+using MailService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailService.Controllers
+{
+    [Route("api/mail")]
+    [ApiController]
+    public class EmailController: ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(
+            IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+        [HttpPost("send")]
+        public IActionResult SendMail(SendMailDto mail)
+        {
+            return Ok(_emailService.sendEmail(mail));
+        }
+    }
+    
+    
+}
---
+++ MailService/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace MailService.Models
+{
+    public class Email
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
