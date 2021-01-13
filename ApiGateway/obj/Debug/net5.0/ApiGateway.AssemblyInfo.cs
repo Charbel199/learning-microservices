597e4fbfd9752b85530af2383039087cdd3b69eb
charbel <charbel-boumaroun@outlook.com>
2021-01-13 19:26:08 +0200

    fix: Fixed HTTPS

--- ApiGateway/obj/Debug/net5.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net5.0/ApiGateway.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("MyNewUniqueSecret")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
