c84f73ed961990eb6a6e3a12685f01af8f4e1132
charbel <charbel-boumaroun@outlook.com>
2021-01-21 18:27:26 +0200

    feat: configuring http service

---
+++ WebsiteUI/src/app/core/params.ts
@@ -0,0 +1,11 @@
+export enum ApiMethod{
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE'
+}
+
+
+export enum EndPoints{
+
+}
--- WebsiteUI/src/app/core/services/http/http.service.ts
+++ WebsiteUI/src/app/core/services/http/http.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ApiMethod, EndPoints} from '../../params';
+import { environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public requestCall<responseType>(api: EndPoints, method: ApiMethod, payload?: any): any{
+    let response;
+    switch (method) {
+      case ApiMethod.GET:
+        response = this.httpClient.get<responseType>(`${environment.apiUrl}/${api}`,
+          {
+            withCredentials: true
+          });
+        break;
+      case ApiMethod.POST:
+        response = this.httpClient.post<responseType>(`${environment.apiUrl}/${api}`, payload,
+          {
+            withCredentials: true
+          });
+        break;
+      case ApiMethod.DELETE:
+        response = this.httpClient.delete<responseType>(`${environment.apiUrl}/${api}`,
+          {
+            withCredentials: true
+          });
+        break;
+      case ApiMethod.PUT:
+        response = this.httpClient.put<responseType>(`${environment.apiUrl}/${api}`, payload,
+          {
+            withCredentials: true
+          });
+        break;
+      default:
+        break;
+    }
+    return response;
+  }
 }
