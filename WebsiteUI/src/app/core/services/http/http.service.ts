b30e646380d8cbceb5aef25be88739bea802b296
charbel <charbel-boumaroun@outlook.com>
2021-01-21 18:37:30 +0200

    feat: error handling (Needs work)

--- WebsiteUI/src/app/core/services/http/http.service.ts
+++ WebsiteUI/src/app/core/services/http/http.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {ApiMethod, EndPoints} from '../../params';
 import { environment} from '../../../../environments/environment';
+import {catchError} from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,29 +21,36 @@ export class HttpService {
         response = this.httpClient.get<responseType>(`${environment.apiUrl}/${api}`,
           {
             withCredentials: true
-          });
+          }).pipe(catchError(err => this.handleError(err)));
         break;
       case ApiMethod.POST:
         response = this.httpClient.post<responseType>(`${environment.apiUrl}/${api}`, payload,
           {
             withCredentials: true
-          });
+          }).pipe(catchError(err => this.handleError(err)));;
         break;
       case ApiMethod.DELETE:
         response = this.httpClient.delete<responseType>(`${environment.apiUrl}/${api}`,
           {
             withCredentials: true
-          });
+          }).pipe(catchError(err => this.handleError(err)));;
         break;
       case ApiMethod.PUT:
         response = this.httpClient.put<responseType>(`${environment.apiUrl}/${api}`, payload,
           {
             withCredentials: true
-          });
+          }).pipe(catchError(err => this.handleError(err)));;
         break;
       default:
         break;
     }
     return response;
+  }
+
+
+  // Should add functionality
+  public handleError(error: HttpErrorResponse): any{
+    console.error(error);
+    return throwError( {error: error.message, status: error.status});
   }
 }
